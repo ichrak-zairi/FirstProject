c6e9227034559949e4b250076212da57acc423f3
ichrak-zairi <146008360+ichrak-zairi@users.noreply.github.com>
2023-10-22 21:16:11 +0200

    <book>

--- src/Controller/BookController.php
+++ src/Controller/BookController.php
@@ -1,15 +1,16 @@
 <?php
 
 namespace App\Controller;
-
+use App\Entity\Book;
 use App\Form\BookType;
+use App\Form\AuthorType;
+
 use App\Repository\BookRepository;
+use Symfony\Component\HttpFoundation\Request;
+use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Component\HttpFoundation\Request;
-use Doctrine\Persistence\ManagerRegistry;
-use App\Entity\Book;
 
 class BookController extends AbstractController
 {
@@ -20,26 +21,68 @@ class BookController extends AbstractController
             'controller_name' => 'BookController',
         ]);
     }
-  
-    #[Route('/book/add2',name:'add2_book')]
-   public function add2 (ManagerRegistry $doctrine,Request $request) {
-    $em= $doctrine->getManager();
-    $book = new book();
-    $form=$this->createForm(BookType::class,$book);
-    $form->handleRequest($request);
+    #[Route('/book/add2',name : 'add2_book')]
+public function add2 (ManagerRegistry $doctrine , Request $request)
+{
+    $em = $doctrine->getManager();
+    $book = new Book();
+  $form = $this->createForm(BookType::class,$book);
+  $form->handleRequest($request);
+ 
+  if ($form->isSubmitted())
+  { 
     $book->setPublished(true);
-    if($form ->isSubmitted()){ 
-    $em->persist($book);
-    $em->flush();
-   return $this->redirectToRoute('read_book');
-    } else {
-        return $this->renderForm('book/addbook.html.twig',['f'=>$form]);
-    }
-   }
-   #[Route('/book/readb',name:'read_book')]
-   public function readb (BookRepository $bookrepo) {
-    $books = $bookrepo->findAll();
-    return $this->render('book/readb.hmtl.twig',['books'=> $books]);
+$em->persist($book);
+$em->flush();
+return $this->redirectToRoute('read_book');
+}
+else
+{
+   return $this->renderForm('book/addbook.html.twig',['f'=>$form]);
+}
+}
+#[Route('/book/read',name:'read_book')]
+public function read (BookRepository $bookrepo) {
+ $book = $bookrepo->findAll();
+ return $this->render('book/read.hmtl.twig',['books'=> $book]);
+}
+#[Route('/book/edit/{id}', name: 'edit_book')]
+public function edit(ManagerRegistry $doctrine, BookRepository $bookrepo, Request $request, $id)
+{
+    $book = $bookrepo->find($id);
+    $em = $doctrine->getManager();
+    $form = $this->createForm(BookType::class, $book);
 
-   }
+    $form->handleRequest($request);
+    if ($form->isSubmitted()) {
+        $em->persist($book);
+        $em->flush();
+        return $this->redirectToRoute("read_book");
+    }
+
+    return $this->renderForm("book/edit.html.twig", ["form" => $form]);
+}
+#[Route('/book/delete/{id}', name: 'delete_book')]
+public function delete(ManagerRegistry $doctrine, BookRepository $bookrep, $id)
+{
+    $em = $doctrine->getManager();
+    $book = $bookrep->find($id);
+
+    $em->remove($book);
+    $em->flush();
+
+    return $this->redirectToRoute("read_book");
+}
+#[Route('/book/show/{id}', name: 'show_book')]
+public function showBook(int $id, BookRepository $bookRepository): Response
+{
+    $book = $bookRepository->find($id);
+
+    if (!$book) {
+        throw $this->createNotFoundException('Le livre demandé n\'existe pas.');
+    }
+
+    return $this->render('book/show.html.twig', ['book' => $book]);
+}
+
 }
--- src/Form/BookType.php
+++ src/Form/BookType.php
@@ -1,16 +1,16 @@
 <?php
 
 namespace App\Form;
-
-use App\Entity\Author;
-use App\Entity\Book;
-use Symfony\Bridge\Doctrine\Form\Type\EntityType;
-use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use App\Entity\Author;
 use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Doctrine\ORM\Mapping\Entity;
+use App\Entity\Book;
+use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
-
 
 class BookType extends AbstractType
 {
@@ -19,18 +19,24 @@ class BookType extends AbstractType
         $builder
             ->add('id')
             ->add('title')
-            ->add('category', ChoiceType::class, [
+            ->add('category',ChoiceType::class,[
                 'choices' => [
-                    'Science-Fiction' => 'Science-Fiction',
-                    'Mystery' => 'Mystery',
-                    'Autobiography' => 'Autobiography',
+                    'science-fiction'=>'science-fiction',
+                    'mystery'=>'mystery',
+                    'autobiography'=> 'autobiography'
+
                 ],
             ])
-            ->add('publicationDate', DateType::class, [
-                'widget' => 'single_text', 
-                'format' => 'yyyy-MM-dd', 
+            ->add('author',EntityType::class,[
+                'class'=>'use App\Entity\Author',
+                'choice_label'=>'username',
+                'multiple'=>false,
+                'expanded' =>false,
+                'required' => true,
+                'placeholder' =>'choisir un auteur',
             ])
             ->add('published')
+            ->add('publicationDate')
             ->add('author', EntityType::class, [
                 'class' => Author::class, 
                 'choice_label' => 'username', 
@@ -38,9 +44,10 @@ class BookType extends AbstractType
                 'expanded'=>false,
                 'required'=> true,
                 'placeholder'=>'choisir un auteur',
-            ]);
+            ])
 
-        ;
+
+            ->add('save',SubmitType::class);
     }
 
     public function configureOptions(OptionsResolver $resolver): void
@@ -49,4 +56,5 @@ class BookType extends AbstractType
             'data_class' => Book::class,
         ]);
     }
+    
 }
